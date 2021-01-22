dd1118a6dd1a38a21f967c085ce3d0b90814d6bf
Mia <ruanya2018@163.com>
2021-01-22 21:58:15 +0800

    Update 69. x 的平方根.go

--- Week_04/69. x 的平方根.go
+++ Week_04/69. x 的平方根.go
@@ -13,7 +13,7 @@
 *
 *	3.2.由于x平方根的整数部分a是满足a^2 <= x,因此我们可以对a进行二分查找，从而得到答案；
 *
-*	3.3.二分查找的下界为0，上界可以粗略地设定为x;(二分查找的思路和代码模板)
+*	3.3.二分查找的下界为0，上界可以粗略地设定为x（x优化：可以直接定为x/2（推导出a <=根号x <= x/2）,缩小二分查找范围）;(二分查找的思路和代码模板)
 *
 *	3.4.在二分查找的每一步中，我们只需要比较中间元素mid的平方与x的大小关系，通过比较的结果不断缩小上下界的范围，直到找到答案.
 *
@@ -27,18 +27,24 @@
 *   空间：O(1)；常量空间
 *
  */
+
 func mySqrt(x int) int {
-	l, r := 0, x
-	ans := -1
+	if x == 1 {
+		return 1
+	}
 
-	for l <= r {
-		mid := l + (r-l)/2
+	left, right := 0, x/2 //优化迭代：right=x改成=x/2,进一步缩小查找范围（可以推导出：a <=根号x <= x/2（x!=1）, 所以right可以取x/2）
+	ans := -1             //ans用来存储结果（x的平方根）
 
-		if mid*mid <= x {
-			ans = mid
-			l = mid + 1
-		} else {
-			r = mid - 1
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if mid <= x/mid { //mid小了，往右找； mid^2小于x，则继续找右边：找更大的，使mid^2值接近x
+			ans = mid      //记录当前小于x的mid值
+			left = mid + 1 //向右查找
+
+		} else { //mid大了，往左找； mid^2大于x，则继续找左边，找更小的，使mid^2值小于等于x
+			right = mid - 1
 		}
 	}
 
@@ -46,3 +52,73 @@ func mySqrt(x int) int {
 }
 
 // 2021年1月22日16:37:33
+
+
+
+
+// 第二遍 		2021年1月22日19:31:16
+
+// 2021年1月22日21:55:08
+
+
+if (x <= 1) {
+	return x;
+}
+int left = 1, right = x / 2;
+
+
+Initialize right = x/2
+Square root of x is always lesser than or equal to x/2.
+
+
+Nice process to prevent the integer overflow:
+
+int mid = left + (right - left)/2;
+mid>x/mid instead of mid*mid>x
+
+
+
+
+
+
+
+
+
+// 国际most vote1
+public int sqrt(int x) {
+    if (x == 0)
+		return 0;
+		
+	int left = 1, right = Integer.MAX_VALUE;
+	
+    while (true) {
+        int mid = left + (right - left)/2;
+        if (mid > x/mid) { /* same as mid * mid > x but prevents overflow */
+            right = mid - 1;
+        } else {
+            if (mid + 1 > x/(mid + 1))
+                return mid;
+            left = mid + 1;
+        }
+    }
+}
+
+
+// 国际most vote2
+class Solution {
+	public:
+		int sqrt(int x) {
+			if (0 == x) return 0;
+			int left = 1, right = x, ans;
+			while (left <= right) {
+				int mid = left + (right - left) / 2;
+				if (mid <= x / mid) {
+					left = mid + 1;
+					ans = mid;
+				} else {
+					right = mid - 1;
+				}
+			}
+			return ans;
+		}
+	};
